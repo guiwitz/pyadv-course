a1d996faf73adcfc45594d623ac07b14a7ff546d
guiwitz <guiwitz@users.noreply.github.com>
2024-06-06 12:17:39 +0200

    add tests

--- tests/test_pyadv_course.py
+++ tests/test_pyadv_course.py
@@ -1,2 +1,15 @@
-def test_something():
-    pass
+from pyadv_course import algos
+import numpy as np
+
+def test_add_one():
+    
+    x = 1
+    out = algos.add_one(x)
+    assert out == 2
+
+def test_add_one_list():
+
+    x = [1,2,3]
+    out = algos.add_one(x)
+    np.testing.assert_array_equal(out, [2,3,4])
+
