7b9d6d6bae0a3eb7d9927f164ec1c44966826999
guiwitz <guiwitz@users.noreply.github.com>
2024-06-07 23:37:02 +0200

    add parametric test

--- tests/test_pyadv_course.py
+++ tests/test_pyadv_course.py
@@ -1,5 +1,6 @@
 from pyadv_course import algos
 import numpy as np
+import pytest
 
 def test_add_one():
     
@@ -12,4 +13,12 @@ def test_add_one_list():
     x = [1,2,3]
     out = algos.add_one(x)
     np.testing.assert_array_equal(out, [2,3,4])
+
+@pytest.mark.parametrize("test_input, expected", 
+                         [([1, 2], [2, 3]), ([1.1, 2.2], [2.1, 3.2])])
+def test_add_multiple(test_input, expected):
+
+    out = algos.add_one(test_input)
+    np.testing.assert_array_equal(out, expected)
+
 
